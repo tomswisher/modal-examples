567e446174a86ee3c563e387c8ae3542df06abf1
Advay Pal <advay@modal.com>
2024-09-20 16:18:05 -0400

    Remove commented code

--- 06_gpu_and_ml/openai_whisper/asr/test_script.py
+++ 06_gpu_and_ml/openai_whisper/asr/test_script.py
@@ -1,24 +1,3 @@
-# import requests
-# import time
-# from random import randbytes
-
-# URL = 'https://modal-labs--latency-test-crazy-web.modal.run'
-# long = randbytes(12 * 1024 * 1024)
-# short = randbytes(6 * 1024 * 1024)
-
-# # session = requests.Session()
-
-# # #warmup tcp connection
-# # for _ in range(5):
-# #     session.post(URL, data=long)
-
-# start = time.monotonic()  
-# requests.post(URL, files = {'file': ('wavs/thirty.wav', open('wavs/thirty.wav', 'rb'), 'audio/wav')})
-# # requests.post(URL, data=short)
-# end = time.monotonic()
-# print(f"Time taken: {end - start}")
-
-
 import requests
 import time
 from random import randbytes
