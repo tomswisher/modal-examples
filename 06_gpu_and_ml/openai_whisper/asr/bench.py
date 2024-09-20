563c9b69b3dba49395e65e24190bd4bd0997800b
Advay Pal <advay@modal.com>
2024-09-20 15:38:22 -0400

    resolve conflict

--- 06_gpu_and_ml/openai_whisper/asr/bench.py
+++ 06_gpu_and_ml/openai_whisper/asr/bench.py
@@ -1,10 +1,10 @@
 import pytest
 import requests
 import json
+import time
 
 # Define URL and audio files
-URL = 'https://modal-labs--low-latency-transcription-model-web-dev.modal.run'
-URL = 'https://modal-labs--faster-v2-model-web-dev.modal.run'
+URL = 'https://modal-labs--latency-test-web.modal.run'
 
 AUDIO_FILES = {
     'thirty': 'wavs/thirty.wav',
@@ -14,25 +14,39 @@ AUDIO_FILES = {
 }
 
 SCENARIOS = [  # (audio_type, batch_size, assisted)
-    ('short', '24', 'false'),
-    ('short', '1', 'true'),
-    ('long', '24', 'false'),
-    ('long', '1', 'true'),
-    ('preamble', '24', 'false'),
-    ('preamble', '1', 'true'),
+    # ('short', '24', 'false'),
+    # ('short', '1', 'true'),
+    # ('long', '24', 'false'),
+    # ('long', '1', 'true'),
+    # ('preamble', '24', 'false'),
+    # ('preamble', '1', 'true'),
+    # ('short', '24', 'false'),
+    # ('preamble', '1', 'false'),
+    # ('long', '24', 'false'),
+    # ('long', '1', 'false'),
+    # ('preamble', '24', 'false'),
+    # ('preamble', '1', 'true'),
     ('thirty', '1', 'false')
 ]
+        
+
+
 
 # Fixture to perform HTTP POST request
 @pytest.fixture
 def perform_request(benchmark):
-
     def _perform_request(audio_type, batch_size, assisted, bench=True):
+        session = requests.Session()
+        for _ in range(5):
+            print(f"Starting request at {time.monotonic()}")
+            session.post(URL, files = {'file': (AUDIO_FILES['long'], open(AUDIO_FILES['long'], 'rb'), 'audio/wav')}, data=None)
+        
         def fn():
             files = {'file': (AUDIO_FILES[audio_type], open(AUDIO_FILES[audio_type], 'rb'), 'audio/wav')}
             parameters = {'batch_size': batch_size, 'assisted': assisted}
             data = {'parameters': json.dumps(parameters)}
-            return requests.post(URL, files=files, data=data)
+
+            return session.post(URL, files=files, data=data)
         if bench:
             return benchmark(fn)
         else:
@@ -45,17 +59,17 @@ def perform_request(benchmark):
 def test_bench(perform_request, audio_type, batch_size, assisted):
     result = perform_request(audio_type, batch_size, assisted)
     assert result.status_code == 200  # Assert successful response
-    print(json.dumps(result.json(), indent=4))  # print response
 
-@pytest.mark.parametrize("audio_type, batch_size, assisted", SCENARIOS)
-def test_singleflight(perform_request, audio_type, batch_size, assisted):
-    result = perform_request(audio_type, batch_size, assisted, bench=False)
-    assert result.status_code == 200  # Assert successful response
-    print(json.dumps(result.json(), indent=4))  # print response
+# @pytest.mark.parametrize("audio_type, batch_size, assisted", SCENARIOS)
+# def test_singleflight(perform_request, audio_type, batch_size, assisted):
+#     result = perform_request(audio_type, batch_size, assisted, bench=False)
+#     assert result.status_code == 200  # Assert successful response
+#     print(json.dumps(result.json(), indent=4))  # print response
 
 # curl -X POST 'https://irfansharif--diarization-model-web.modal.run' \
 #     -F 'file=@wavs/short.wav;type=audio/wav' \
 #     -F 'parameters={"batch_size":24,"assisted":false}'
 
 
-
+# if __name__ == "__main__":
+#     main()
